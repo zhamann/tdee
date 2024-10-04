f15b01915ea630f89c0c4d73fd88ce417353b595
Zac Hamann <hamannzac@gmail.com>
2024-10-04 13:51:08 -0500

    adjust viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import type { Viewport, Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "TDEE Calculator",
   description: "Total Daily Energy Expenditure (TDEE) Calculator.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
